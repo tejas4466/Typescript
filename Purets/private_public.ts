f45156ed9a14e1eefc9da1e402457852b828db98
tejas4466 <tejaspokale22@gmail.com>
2024-10-15 01:51:18 +0530

    done

---
+++ Purets/private_public.ts
@@ -0,0 +1,11 @@
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+
+//     }
+// }
+
